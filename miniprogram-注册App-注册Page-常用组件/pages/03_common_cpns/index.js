15864c2c6a75d8eab8b37ce6beef16ffcfdfa8a4
wudang <dang.wu@melot.cn>
2024-09-11 16:39:32 +0800

    常用内置组件：text button image scroll-view

--- miniprogram-注册App-注册Page-常用组件/pages/03_common_cpns/index.js
+++ miniprogram-注册App-注册Page-常用组件/pages/03_common_cpns/index.js
@@ -1,66 +1,20 @@
 // pages/03_common_cpns/index.js
 Page({
-
-  /**
-   * Page initial data
-   */
-  data: {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page load
-   */
-  onLoad(options) {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page is initially rendered
-   */
-  onReady() {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page show
-   */
-  onShow() {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page hide
-   */
-  onHide() {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page unload
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * Page event handler function--Called when user drop down
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * Called when page reach bottom
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * Called when user click on the top right corner to share
-   */
-  onShareAppMessage() {
-
-  }
+    data:{
+        colors:['red','green','blue','orange'],
+        chooseImage:''
+    },
+    onClick(event){
+        const color = event.target.dataset.item
+        console.log(color,'被点击')
+    },
+    chooseImage(){
+        wx.chooseMedia({
+            mediaType:'image'
+        }).then(rep=>{
+            console.log(rep)
+            const chooseImagePath = rep.tempFiles[0].tempFilePath
+            this.setData({chooseImage:chooseImagePath})
+        })
+    }
 })
